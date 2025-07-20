23b4019ef3a0331aaeb84b25a9de75502576a349
Aditya Roy <adityaroy712249@gmail.com>
2025-07-20 16:03:32 +0530

    fix: bug fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,17 +12,7 @@ const Footer = () => {
           Stay Connected with Your Alumni Community
         </p>
       </div>
-    <section className="flex flex-col justify-center px-6 md:px-12 py-16 bg-gradient-to-r from-red-700 via-purple-800 to-[#100857] text-white h-[40vh]">
-      <h1 className="text-xl md:text-2xl font-[caveat] italic mb-2">
-        EchoAlma
-      </h1>
-      <h2 className="text-3xl md:text-5xl font-[crimson] font-bold mb-2">
-        Alumni Association
-      </h2>
-      <p className="text-base md:text-lg">
-        Stay Connected with Your Alumni Community
-      </p>
-    </section>
+    </section>    
   );
 };
 
